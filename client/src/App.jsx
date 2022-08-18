5fcb24189809f4f5846024a3ddb1091213cf37a3
Ваше Имя <you@example.com>
2022-08-18 19:27:35 +0300

    save navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import Header from './Components/Header/Header';
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<h1>hello</h1>} />
+          <Route path="/post" element={<h1>post</h1>} />
+        </Routes>
+      </Container>
+    </>
   );
 }
