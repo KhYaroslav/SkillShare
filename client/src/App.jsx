7415fdcc526132172b2362432e0eb55b3257d749
KH.Yaroslav <icpr.johni@gmail.com>
2022-08-19 00:05:23 +0300

    save

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Container from '@mui/material/Container';
-import Header from './Components/Header/Header';
+import {
+  Box, createTheme, Stack, ThemeProvider
+} from '@mui/material';
+import { useState } from 'react';
+import Sidebar from './Components/AddBar/Sidebar/Sidebar';
+import Feed from './Components/Feed/Feed';
+import Rightbar from './Components/AddBar/Rightbar/Rightbar';
+import Navbar from './Components/AddBar/Navbar/Navbar';
+import Add from './Components/Add/Add';
 
-export default function App() {
+function App() {
+  const [mode, setMode] = useState('light');
+
+  const darkTheme = createTheme({
+    palette: {
+      mode,
+    },
+  });
   return (
-    <>
-      <Header />
-      <Container maxWidth="lg">
-        <Routes>
-          <Route path="/" element={<h1>hello</h1>} />
-          <Route path="/post" element={<h1>post</h1>} />
-        </Routes>
-      </Container>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor="background.default" color="text.primary">
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
+
+export default App;
---
+++ client/src/Components/AddBar/Sidebar/Sidebar.jsx
@@ -0,0 +1,73 @@
+import {
+  AccountBox,
+  Article,
+  Group,
+  Home,
+  ModeNight,
+  Person,
+  Settings,
+  Storefront,
+} from '@mui/icons-material';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from '@mui/material';
+import React from 'react';
+
+const Sidebar = ({ mode, setMode }) => {
+  return (
+    <Box flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <Box position="fixed">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#home">
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary="Домой" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary="Пользователи" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary="Настройки" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary="Профиль" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemIcon>
+                <ModeNight />
+              </ListItemIcon>
+              <Switch onChange={(e) => setMode(mode === 'light' ? 'dark' : 'light')} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </Box>
+  );
+};
+
+export default Sidebar;
