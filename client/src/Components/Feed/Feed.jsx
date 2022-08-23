84c1963ce9c50a4e7043f506c8ee5d4159797ccb
Ваше Имя <you@example.com>
2022-08-23 14:26:48 +0300

    search ready

--- client/src/Components/Feed/Feed.jsx
+++ client/src/Components/Feed/Feed.jsx
@@ -2,38 +2,23 @@ import { Box, Stack, Skeleton } from '@mui/material';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addPosts } from '../../Redux/actions/postActions';
+import { allPosts } from '../../Redux/actions/postActions';
 import Post from '../Post/Post';
 
 export default function Feed() {
-  const [loading, setLoading] = useState(true);
-  setTimeout(() => {
-    setLoading(false);
-  }, [80]);
   const posts = useSelector((state) => state.posts);
   const dispatch = useDispatch();
+
   useEffect(() => {
     if (!posts.length) {
-      dispatch(addPosts());
+      dispatch(allPosts());
     }
   }, [posts]);
-  // console.log('posts--!!->', posts);
 
   return (
     <>
       <Box flex={4} p={{ xs: 0, md: 2 }}>
-        {loading ? (
-          <Stack spacing={1}>
-            <Skeleton variant="text" height={100} />
-            <Skeleton variant="text" height={20} />
-            <Skeleton variant="text" height={20} />
-            <Skeleton variant="rectangular" height={300} />
-          </Stack>
-        ) : (
-          <>
-            {posts.length && posts.map((el) => <Post key={el.id} post={el} />)}
-          </>
-        )}
+        {posts.length && posts.map((el) => <Post key={el.id} post={el} />)}
       </Box>
     </>
   );
